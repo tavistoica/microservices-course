bdd1eada06d5d54a8ba5863f3830d163de0d87b5
tavistoica <stoicaoctavian25@yahoo.com>
2021-05-15 00:33:19 +0300

    finished section 12

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
-import { BadRequestError } from "../errors/bad-request-error";
-import { validateRequest } from "../middleware/validate-request";
+import { BadRequestError, validateRequest } from "@omstickets/common";
 import { User } from "../models/user.model";
 import { Password } from "../services/password";
 import jwt from "jsonwebtoken";
