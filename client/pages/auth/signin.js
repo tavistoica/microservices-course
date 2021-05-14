c1e3e16c6ff4f262185c1a770c9e99b9380b0071
tavistoica <stoicaoctavian25@yahoo.com>
2021-05-14 14:56:09 +0300

    finished section 11

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Router from "next/router";
+import useRequest from "../../hooks/use-request";
+
+export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signin",
+    method: "post",
+    body: { email, password },
+    onSuccess: () => {
+      Router.push("/");
+      setEmail("");
+      setPassword("");
+    },
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign up</h1>
+      {errors}
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          className="form-control"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          className="form-control"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </div>
+      <button className="btn btn-primary">Sign In</button>
+    </form>
+  );
+};
