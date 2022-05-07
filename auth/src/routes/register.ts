d6194337813ad226c42ec62c0a111a7119766517
tavistoica <stoicaoctavian25@yahoo.com>
2022-05-07 13:38:18 +0300

    updated auth service and client

---
+++ auth/src/routes/register.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { body } from "express-validator";
+import { validateRequest } from "@omstickets/common";
+import { registerController } from "../controllers/register";
+
+const router = express.Router();
+
+router.post(
+  "/api/users/register",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  validateRequest,
+  registerController
+);
+
+export { router as signUpRouter };
