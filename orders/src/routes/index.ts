7d21bc88fb4f6d7f70d0bb35dc4d295a1aea91a2
tavistoica <stoicaoctavian25@yahoo.com>
2022-05-10 12:24:22 +0300

    modified get /api/orders

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { requireAuth } from "@ostoica/common";
 import { Order } from "../model/order.model";
+import { Ticket } from "../model/ticket.model";
 
 const router = express.Router();
 
@@ -9,7 +10,12 @@ router.get("/api/orders", requireAuth, async (req: Request, res: Response) => {
     userId: req.currentUser!.id,
   });
 
-  res.send(orders);
+  const response = orders.map((item) => {
+    const ticket = Ticket.find({ id: item.ticket });
+    return { ...item, ticket };
+  });
+
+  res.send(response);
 });
 
 export { router as indexOrderRouter };
