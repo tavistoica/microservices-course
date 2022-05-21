9adb6cb6be41e5df67278e58ac2775a3cf0a0d26
tavistoica <stoicaoctavian25@yahoo.com>
2022-05-21 22:18:44 +0300

    added logger

--- auth/src/controllers/register.ts
+++ auth/src/controllers/register.ts
@@ -1,14 +1,17 @@
 import { Request, Response } from "express";
-import { BadRequestError, validateRequest } from "@ostoica/common";
+import { BadRequestError } from "@ostoica/common";
 import { User } from "../models/user.model";
 import jwt from "jsonwebtoken";
+import { logger } from "../utils/logger";
 
 export const registerController = async (req: Request, res: Response) => {
   const { email, password, role } = req.body;
 
   const existingUser = await User.findOne({ email });
+  logger.info(`registerController - existingUser ${existingUser}`);
 
   if (existingUser) {
+    logger.error(`registerController - Email in use`);
     throw new BadRequestError("Email in use");
   }
 
@@ -24,6 +27,7 @@ export const registerController = async (req: Request, res: Response) => {
     },
     process.env.JWT_KEY!
   );
+  logger.info(`registerController - userJWT - ${userJwt}`);
 
   //  Store it on session object
   req.session = { jwt: userJwt };
