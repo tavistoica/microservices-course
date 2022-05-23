79617d94c403a297027e6dc32a5613115f661775
tavistoica <stoicaoctavian25@yahoo.com>
2022-05-23 21:05:36 +0300

    more logging for order messaging

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -3,6 +3,7 @@ import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-croup-name";
 import { Ticket } from "../../models/ticket.model";
 import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
+import { logger } from "../../utils/logger";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -11,6 +12,8 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
     //  Find the ticket that the order is reserving
     const ticket = await Ticket.findById(data.ticket.id);
+    logger.info(`OrderCreatedEvent - ticket version 1 - ${ticket?.version}`);
+    logger.info(`OrderCreatedEvent - ticket - ${JSON.stringify(ticket)}`);
 
     //  If no ticket, throw error
     if (!ticket) {
@@ -23,10 +26,17 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
     //  Mark the ticket as being reserved by setting its orderId property
     const orderId = ticket.orderId ? ticket.orderId.push(data.id) : [data.id];
+    logger.info(
+      `OrderCreatedEvent - orderId - ${JSON.stringify(orderId)} - stock - ${
+        ticket.stock - data.itemAmount
+      }`
+    );
+
     ticket.set({ orderId, stock: ticket.stock - data.itemAmount });
 
     //  Save the ticket
     await ticket.save();
+    logger.info(`OrderCreatedEvent - ticket version 2 - ${ticket.version}`);
     new TicketUpdatedPublisher(this.client).publish({
       id: ticket.id,
       version: ticket.version,
