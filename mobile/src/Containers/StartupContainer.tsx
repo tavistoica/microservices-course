1bf182cf77e1f0ebc9a2bd2d4d74981f831cfffd
Adrian Karp <adrian.karp@ibm.com>
2022-06-02 13:03:35 +0300

    removed unused files and dependencies

--- mobile/src/Containers/StartupContainer.tsx
+++ mobile/src/Containers/StartupContainer.tsx
@@ -1,37 +1,23 @@
-import React, { useEffect } from 'react'
-import { ActivityIndicator, View, Text } from 'react-native'
-import { useTranslation } from 'react-i18next'
-import { useTheme } from '@/Hooks'
-import { Brand } from '@/Components'
-import { setDefaultTheme } from '@/Store/Theme'
-import { navigateAndSimpleReset } from '@/Navigators/utils'
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 const StartupContainer = () => {
-  const { Layout, Gutters, Fonts } = useTheme()
-
-  const { t } = useTranslation()
-
-  const init = async () => {
-    await new Promise(resolve =>
-      setTimeout(() => {
-        resolve(true)
-      }, 2000),
-    )
-    await setDefaultTheme({ theme: 'default', darkMode: null })
-    navigateAndSimpleReset('Main')
-  }
-
-  useEffect(() => {
-    init()
-  })
-
   return (
-    <View style={[Layout.fill, Layout.colCenter]}>
-      <Brand />
-      <ActivityIndicator size={'large'} style={[Gutters.largeVMargin]} />
-      <Text style={Fonts.textCenter}>{t('welcome')}</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Starting Point</Text>
     </View>
   )
 }
 
 export default StartupContainer
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 10,
+  },
+  text: {
+    textAlign: 'center',
+  },
+})
