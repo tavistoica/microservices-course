8737f2a10f5460607183da6ab6b9a61ebfa774f4
tavistoica <stoicaoctavian25@yahoo.com>
2022-06-16 17:25:24 +0300

    restricting a seller from buying meals

--- client/utils/utils.js
+++ client/utils/utils.js
@@ -4,3 +4,11 @@ export const baseURL =
   process.env.IS_PRODUCTION === "false"
     ? HOST_URL.DEV.BROWSER_URL
     : HOST_URL.PROD.BROWSER_URL;
+
+export const calculateTime = (time) => {
+  const hours = time / 60 / 60;
+  const minutes = (time - hours) / 60;
+  const seconds = time - hours - minutes;
+
+  return { hours, minutes, seconds };
+};
