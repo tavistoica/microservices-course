4b680053219f446f499dea5411477ddd6dd40d69
tavistoica <stoicaoctavian25@yahoo.com>
2022-07-31 23:06:46 +0300

    cors specifications + client fixes for new auth

--- client/pages/auth/login.js
+++ client/pages/auth/login.js
@@ -1,20 +1,27 @@
 import { useState } from "react";
 import Router from "next/router";
+import useAuth from "../../hooks/use-auth";
 import useRequest from "../../hooks/use-request";
 
 import { Login } from "../../components/organisms/Login/Login";
 
 const login = () => {
+  const { setAuth, persist, setPersist } = useAuth();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { doRequest, errors } = useRequest({
     url: "/api/users/login",
     method: "post",
     body: { email, password },
-    onSuccess: () => {
-      Router.push("/");
+    onSuccess: (response) => {
+      setAuth({
+        role: response?.data?.role,
+        accessToken: response?.data?.accessToken,
+      });
       setEmail("");
       setPassword("");
+      Router.push("/");
     },
   });
 
@@ -24,6 +31,14 @@ const login = () => {
     await doRequest();
   };
 
+  const togglePersist = () => {
+    setPersist((prev) => !prev);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("persist", persist);
+  }, [persist]);
+
   return (
     <Login
       onSubmit={onSubmit}
@@ -32,6 +47,7 @@ const login = () => {
       password={password}
       setEmail={setEmail}
       setPassword={setPassword}
+      togglePersist={togglePersist}
     />
   );
 };
