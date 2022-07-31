195241db22587e6986b5ada22d5c2207708acfd4
tavistoica <stoicaoctavian25@yahoo.com>
2022-07-31 22:33:27 +0300

    modified ssr data calls for client mx

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -18,22 +18,31 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
   );
 };
 
-AppComponent.getInitialProps = async (appContext) => {
-  if (appContext?.ctx?.req?.headers?.cookie) {
+AppComponent.getServerSideProps = async (context) => {
+  if (context?.ctx?.req?.headers?.cookie) {
     // it runs on server side
-    axios.defaults.headers.get.Cookie = appContext.ctx.req.headers.cookie;
+    axios.defaults.headers.get.Cookie = context.ctx.req.headers.cookie;
   }
-  const { data } = await axios.get("/api/users/currentuser", {
-    withCredentials: true,
-  });
 
-  let pageProps;
-  if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(
-      appContext.ctx,
-      data.currentUser
-    );
+  let data = null;
+  if (context?.ctx?.req?.headers?.cookie) {
+    try {
+      const response = await axios.get("/api/users/currentuser", {
+        withCredentials: true,
+      });
+      data = response;
+    } catch (err) {
+      console.log(err);
+    }
   }
+
+  // let pageProps;
+  // if (context.Component.getInitialProps) {
+  //   pageProps = await context.Component.getInitialProps(
+  //     context.ctx,
+  //     data ? data?.currentUser : null
+  //   );
+  // }
   return { pageProps, ...data };
 };
 
